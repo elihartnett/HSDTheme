02e74d68037be24f51c19bcce85e243c2dcccbc4
Eli Hartnett <eli@elihartnett.com>
2025-02-10 13:24:51 -0700

    Update variable name

--- driver.sh
+++ driver.sh
@@ -9,11 +9,8 @@ fi
 
 THEMES_SOURCE="$1"
 OUTPUT_DESTINATION="$2"
-DRIVER_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
-THEMES_DIRECTORY="$DRIVER_DIRECTORY/StyleDictionary/themes"
-
-echo "$THEMES_SOURCE"
-echo "$THEMES_DIRECTORY"
+SCRIPT_DIRECTORY="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+THEMES_DIRECTORY="$SCRIPT_DIRECTORY/StyleDictionary/themes"
 
 if [ ! -d "$THEMES_SOURCE" ]; then
     echo "Error: Source themes directory '$THEMES_SOURCE' does not exist."
@@ -24,7 +21,7 @@ rm -rf "$THEMES_DIRECTORY"
 mkdir -p "$THEMES_DIRECTORY"
 cp -r "$THEMES_SOURCE"/. "$THEMES_DIRECTORY"/
 
-npm --prefix "$DRIVER_DIRECTORY/StyleDictionary" install
-npm --prefix "$DRIVER_DIRECTORY/StyleDictionary" run build
+npm --prefix "$SCRIPT_DIRECTORY/StyleDictionary" install
+npm --prefix "$SCRIPT_DIRECTORY/StyleDictionary" run build
 
-cp -r "$DRIVER_DIRECTORY/StyleDictionary/build/ios/SDThemer.swift" "$OUTPUT_DESTINATION"
+cp -r "$SCRIPT_DIRECTORY/StyleDictionary/build/ios/SDThemer.swift" "$OUTPUT_DESTINATION"
